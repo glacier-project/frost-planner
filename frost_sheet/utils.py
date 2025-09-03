0fc9ab80890b578be33433859f9dcbb2c30eaf33
Galfurian <enrico.fraccaroli@univr.it>
2025-09-03 13:02:47 -0400

    feature(utils): Add crule function for printing rules

--- frost_sheet/utils.py
+++ frost_sheet/utils.py
@@ -1,6 +1,6 @@
 from typing import Any
 from rich.console import Console
-
+from rich.rule import Rule
 
 # Initialize the rich console.
 _console = Console(markup=True, width=120, force_terminal=True, force_jupyter=False)
@@ -37,3 +37,14 @@ def cwarning(*args: Any, **kwargs: Any) -> None:
             This function does not return any value.
     """
     _console.print(*args, style="bold yellow", **kwargs)
+
+
+def crule(*args: Any, **kwargs: Any) -> None:
+    """
+    Custom print function for rules.
+
+    Returns:
+        None:
+            This function does not return any value.
+    """
+    _console.print(Rule(*args, **kwargs))
