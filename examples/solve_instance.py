03e62d03fb966796c13d668deeebf3895e569615
Sebastiano <gaia.franseba@gmail.com>
2025-09-04 10:00:05 +0200

    fix(examples): Update default value in solve_instance.py

--- examples/solve_instance.py
+++ examples/solve_instance.py
@@ -31,7 +31,7 @@ def parse_args() -> argparse.Namespace:
         "-i",
         "--instance",
         type=str,
-        default="resources/instances/easy/instance_0.json",
+        default="data/instance_0.json",
         help="Instance configuration to use",
     )
     parser.add_argument(
